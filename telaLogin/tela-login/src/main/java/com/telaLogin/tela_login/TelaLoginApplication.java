f110c4ab3aaa4cc650c401d444445fe6870f6e50
Maria Luiza Ribeiro de Lima <mlrlima04@gmail.com>
2026-06-14 23:07:41 -0300

    .

---
+++ telaLogin/tela-login/src/main/java/com/telaLogin/tela_login/TelaLoginApplication.java
@@ -0,0 +1,13 @@
+package com.telaLogin.tela_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelaLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelaLoginApplication.class, args);
+	}
+
+}
