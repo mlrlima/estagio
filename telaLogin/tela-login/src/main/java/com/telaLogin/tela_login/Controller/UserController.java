dc91a23fbc57924de70419a24eae17c6f3151e6c
Maria Luiza Lima <132663650+mlrlima@users.noreply.github.com>
2026-06-29 23:26:30 -0300

    Delete telaLogin/tela-login directory

--- telaLogin/tela-login/src/main/java/com/telaLogin/tela_login/Controller/UserController.java
+++
@@ -1,5 +0,0 @@
-package com.telaLogin.tela_login.Controller;
-
-public class UserController{
-	
-}
--- telaLogin/tela-login/src/main/java/com/telaLogin/tela_login/Service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.telaLogin.tela_login.Service;
-
-@Service
-public class UserService{
-	
-	private UserRepository repository;
-}
